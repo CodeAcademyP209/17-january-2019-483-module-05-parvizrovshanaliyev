488f75242d4af374eecdc6e296ce8b377672ab4e
parvizrovshanaliyev <parvizra@code.edu.az>
2019-01-18 12:57:21 +0400

    works

---
+++ CompanyController/CompanyController/AddWorkerPanel.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace CompanyController
+{
+    partial class AddWorkerPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewAddWorker = new System.Windows.Forms.DataGridView();
+            this.dateTimePickerRegister = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbxEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxNumber = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxPay = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxWork = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxSurname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnAddWorkerTolist = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAddWorker)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.btnAddWorkerTolist);
+            this.groupBox1.Controls.Add(this.dataGridViewAddWorker);
+            this.groupBox1.Controls.Add(this.dateTimePickerRegister);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.tbxEmail);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbxNumber);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.tbxPay);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbxWork);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbxSurname);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbxName);
+            this.groupBox1.Location = new System.Drawing.Point(32, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1229, 640);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "add worker";
+            // 
+            // dataGridViewAddWorker
+            // 
+            this.dataGridViewAddWorker.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridViewAddWorker.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAddWorker.Location = new System.Drawing.Point(329, 21);
+            this.dataGridViewAddWorker.Name = "dataGridViewAddWorker";
+            this.dataGridViewAddWorker.RowTemplate.Height = 24;
+            this.dataGridViewAddWorker.Size = new System.Drawing.Size(881, 601);
+            this.dataGridViewAddWorker.TabIndex = 17;
+            // 
+            // dateTimePickerRegister
+            // 
+            this.dateTimePickerRegister.Location = new System.Drawing.Point(119, 439);
+            this.dateTimePickerRegister.Name = "dateTimePickerRegister";
+            this.dateTimePickerRegister.Size = new System.Drawing.Size(194, 22);
+            this.dateTimePickerRegister.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 442);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Register Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Email";
+            // 
+            // tbxEmail
+            // 
+            this.tbxEmail.Location = new System.Drawing.Point(119, 389);
+            this.tbxEmail.Name = "tbxEmail";
+            this.tbxEmail.Size = new System.Drawing.Size(194, 22);
+            this.tbxEmail.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Number";
+            // 
+            // tbxNumber
+            // 
+            this.tbxNumber.Location = new System.Drawing.Point(119, 342);
+            this.tbxNumber.Name = "tbxNumber";
+            this.tbxNumber.Size = new System.Drawing.Size(194, 22);
+            this.tbxNumber.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Pay";
+            // 
+            // tbxPay
+            // 
+            this.tbxPay.Location = new System.Drawing.Point(119, 296);
+            this.tbxPay.Name = "tbxPay";
+            this.tbxPay.Size = new System.Drawing.Size(194, 22);
+            this.tbxPay.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Work";
+            // 
+            // tbxWork
+            // 
+            this.tbxWork.Location = new System.Drawing.Point(119, 249);
+            this.tbxWork.Name = "tbxWork";
+            this.tbxWork.Size = new System.Drawing.Size(194, 22);
+            this.tbxWork.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Surname";
+            // 
+            // tbxSurname
+            // 
+            this.tbxSurname.Location = new System.Drawing.Point(119, 202);
+            this.tbxSurname.Name = "tbxSurname";
+            this.tbxSurname.Size = new System.Drawing.Size(194, 22);
+            this.tbxSurname.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(119, 151);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(194, 22);
+            this.tbxName.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnAddWorkerTolist
+            // 
+            this.btnAddWorkerTolist.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAddWorkerTolist.Location = new System.Drawing.Point(167, 480);
+            this.btnAddWorkerTolist.Name = "btnAddWorkerTolist";
+            this.btnAddWorkerTolist.Size = new System.Drawing.Size(146, 33);
+            this.btnAddWorkerTolist.TabIndex = 18;
+            this.btnAddWorkerTolist.Text = "Add Worker To list";
+            this.btnAddWorkerTolist.UseVisualStyleBackColor = false;
+            this.btnAddWorkerTolist.Click += new System.EventHandler(this.btnAddWorkerTolist_Click);
+            // 
+            // AddWorkerPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1296, 714);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "AddWorkerPanel";
+            this.Text = "AddWorkerPanel";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAddWorker)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridViewAddWorker;
+        private System.Windows.Forms.DateTimePicker dateTimePickerRegister;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbxEmail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxNumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxPay;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxWork;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxSurname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnAddWorkerTolist;
+    }
+}
---
+++ CompanyController/CompanyController/CompanyUniversalPanel.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace CompanyController
+{
+    partial class CompanyUniversalPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnclearlist = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSalaryCalcPanel = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.btnAllWorkers = new System.Windows.Forms.Button();
+            this.btnDeleteWorker = new System.Windows.Forms.Button();
+            this.btnAddWorker = new System.Windows.Forms.Button();
+            this.dataGridViewUniversal = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUniversal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.groupBox1.Controls.Add(this.btnclearlist);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.dataGridViewUniversal);
+            this.groupBox1.Location = new System.Drawing.Point(12, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1519, 695);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Welcome";
+            // 
+            // btnclearlist
+            // 
+            this.btnclearlist.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnclearlist.Location = new System.Drawing.Point(341, 623);
+            this.btnclearlist.Name = "btnclearlist";
+            this.btnclearlist.Size = new System.Drawing.Size(75, 39);
+            this.btnclearlist.TabIndex = 12;
+            this.btnclearlist.Text = "Clear";
+            this.btnclearlist.UseVisualStyleBackColor = false;
+            this.btnclearlist.Click += new System.EventHandler(this.btnclearlist_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Maasi maas ortalamasindan cox olan torpilliler"});
+            this.listBox1.Location = new System.Drawing.Point(29, 514);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(294, 148);
+            this.listBox1.TabIndex = 11;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox2.Controls.Add(this.btnSalaryCalcPanel);
+            this.groupBox2.Controls.Add(this.btnAverage);
+            this.groupBox2.Controls.Add(this.btnAllWorkers);
+            this.groupBox2.Controls.Add(this.btnDeleteWorker);
+            this.groupBox2.Controls.Add(this.btnAddWorker);
+            this.groupBox2.ForeColor = System.Drawing.Color.Black;
+            this.groupBox2.Location = new System.Drawing.Point(795, 519);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(692, 143);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "control panel";
+            // 
+            // btnSalaryCalcPanel
+            // 
+            this.btnSalaryCalcPanel.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSalaryCalcPanel.Location = new System.Drawing.Point(50, 89);
+            this.btnSalaryCalcPanel.Name = "btnSalaryCalcPanel";
+            this.btnSalaryCalcPanel.Size = new System.Drawing.Size(169, 39);
+            this.btnSalaryCalcPanel.TabIndex = 10;
+            this.btnSalaryCalcPanel.Text = "Hourly Salary Calculate ";
+            this.btnSalaryCalcPanel.UseVisualStyleBackColor = false;
+            this.btnSalaryCalcPanel.Click += new System.EventHandler(this.btnSalaryCalcPanel_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAverage.Location = new System.Drawing.Point(241, 89);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 39);
+            this.btnAverage.TabIndex = 7;
+            this.btnAverage.Text = "Average";
+            this.btnAverage.UseVisualStyleBackColor = false;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // btnAllWorkers
+            // 
+            this.btnAllWorkers.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAllWorkers.Location = new System.Drawing.Point(442, 41);
+            this.btnAllWorkers.Name = "btnAllWorkers";
+            this.btnAllWorkers.Size = new System.Drawing.Size(136, 42);
+            this.btnAllWorkers.TabIndex = 9;
+            this.btnAllWorkers.Text = "All Workers";
+            this.btnAllWorkers.UseVisualStyleBackColor = false;
+            this.btnAllWorkers.Click += new System.EventHandler(this.btnAllWorkers_Click);
+            // 
+            // btnDeleteWorker
+            // 
+            this.btnDeleteWorker.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDeleteWorker.Location = new System.Drawing.Point(522, 89);
+            this.btnDeleteWorker.Name = "btnDeleteWorker";
+            this.btnDeleteWorker.Size = new System.Drawing.Size(164, 39);
+            this.btnDeleteWorker.TabIndex = 8;
+            this.btnDeleteWorker.Text = "Delete worker";
+            this.btnDeleteWorker.UseVisualStyleBackColor = false;
+            this.btnDeleteWorker.Click += new System.EventHandler(this.btnDeleteWorker_Click);
+            // 
+            // btnAddWorker
+            // 
+            this.btnAddWorker.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAddWorker.Location = new System.Drawing.Point(346, 89);
+            this.btnAddWorker.Name = "btnAddWorker";
+            this.btnAddWorker.Size = new System.Drawing.Size(138, 39);
+            this.btnAddWorker.TabIndex = 6;
+            this.btnAddWorker.Text = "Add Worker";
+            this.btnAddWorker.UseVisualStyleBackColor = false;
+            this.btnAddWorker.Click += new System.EventHandler(this.btnAddWorker_Click);
+            // 
+            // dataGridViewUniversal
+            // 
+            this.dataGridViewUniversal.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dataGridViewUniversal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUniversal.Location = new System.Drawing.Point(29, 32);
+            this.dataGridViewUniversal.Name = "dataGridViewUniversal";
+            this.dataGridViewUniversal.RowTemplate.Height = 24;
+            this.dataGridViewUniversal.Size = new System.Drawing.Size(1458, 458);
+            this.dataGridViewUniversal.TabIndex = 0;
+            // 
+            // CompanyUniversalPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1544, 738);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CompanyUniversalPanel";
+            this.Text = "CompanyUniversalPanel";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUniversal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridViewUniversal;
+        private System.Windows.Forms.Button btnSalaryCalcPanel;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Button btnAllWorkers;
+        private System.Windows.Forms.Button btnDeleteWorker;
+        private System.Windows.Forms.Button btnAddWorker;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnclearlist;
+    }
+}
